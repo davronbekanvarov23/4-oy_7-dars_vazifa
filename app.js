0c3da9ad5f7c5a44d74c794de99879756ea94ab9
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-27 13:02:24 +0500

    added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// 1-MASHQ
+/*--------------------2-AMALIYOT-------------------------*/
+/*--------------------1-MASHQ-------------------------*/
 
 // const ism = prompt("ismni kiriting:");
 // const yosh = +prompt("Yoshni kiriting:");
@@ -15,7 +16,7 @@
 
 // alert(result);
 
-// 2-MASHQ
+/*--------------------2-MASHQ-------------------------*/
 
 // const ism=prompt('Ism kiriting')
 // const ismlar=('Shohruh','Sardor','Davron')
@@ -25,7 +26,7 @@
 // }
 // else{alert(`Afsus ${ism} yo'q`)}
 
-// 3-MASHQ
+/*--------------------3-MASHQ-------------------------*/
 
 // const ismlar = ["Shohruh", "Sardor", "Davron"];
 
@@ -44,7 +45,7 @@
 //   i++;
 // }
 
-// 4-MASHQ
+/*--------------------4-MASHQ-------------------------*/
 
 // const viloyatNomi = prompt("Qaysi viloyat kerak ?");
 
@@ -75,7 +76,7 @@
 //   alert(`Hozircha ba'zada ${viloyatNomi} viloyat mavjud emas`);
 // }
 
-// 5-MASHQ
+/*--------------------5-MASHQ-------------------------*
 
 // const belgilar = prompt("nimadurlar yozing....😊");
 
@@ -94,4 +95,82 @@
 //     }ta belgi kiritdingiz.Bu meyoridan  ${meyor - belgilar.length}ta kam  `
 //   );
 // }
+
+/*--------------------3-AMALIYOT-------------------------*/
+
+/*--------------------1-MASHQ-------------------------*/
+
+// const myArrey = [[false, true], [1, 2], 99, "yaxshi"];
+
+// for
+// for (let i = 0; i < myArrey.length; i++) {
+//   if (typeof myArrey[i] == "string") {
+//     console.log("String index:", i);
+//   }
+// }
+
+// myArrey.forEach((item, i) => {
+//   if (typeof item[i] == "string") {
+//     console.log("String index:", i);
+//   }
+// });
+
+/*--------------------2-MASHQ-------------------------*/
+
+// const data = [[1, 2, 3], "qizil", [4, 5, 6], true, false];
+// const newData = [];
+// data.forEach((item) => {
+//   const res = Array.isArray(item);
+//   if (res) {
+//     item.forEach((num) => {
+//       newData.push(num);
+//     });
+//   }
+// });
+
+// console.log(newData);
+
+/*--------------------3-MASHQ-------------------------*/
+
+// const checkData = [null, "false", 0, 99, false, undefined, "", NaN, "0"];
+// const falsey = [];
+
+// checkData.forEach((item) => {
+//   if (!item) {
+//     falsey.push(item);
+//   }
+// });
+// console.log(falsey);
+
+/*--------------------4-MASHQ-------------------------*/
+// 1-usul
+// const word = "Samarqand";
+// const splitWord = word.split("").reverse();
+// let newWord = "";
+// for (let i = 0; i < splitWord.length; i++) {
+//   newWord += splitWord[i];
+// }
+// console.log(newWord);
+
+// FOREACH
+
+// const word = "Samarqand";
+// const splitWord = word.split("").reverse();
+// let newWord = [];
+// splitWord.forEach((item) => {
+//   newWord.push(item);
+// });
+// console.log(newWord);
+
+// 2-usul
+// const word = "samarqand";
+// const splitWord = word.split("");
+// let newWord = "";
+
+// for (let i = word.length - 1; i >= 0; i--) {
+//   newWord += splitWord[i];
+// }
+// console.log(newWord);
+
+/*--------------------5-MASHQ-------------------------*/
 
