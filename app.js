8f41aa1c207b0153d1e6aa5a511bbbb64f8fcb33
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-27 12:10:46 +0500

    changed

--- app.js
+++ app.js
@@ -1,2 +1,79 @@
-const ism=prompt('ismni kiriting:')
-const yosh=prompt('Yoshni kiriting:')
+// 1-MASHQ
+
+// const ism = prompt("ismni kiriting:");
+// const yosh = +prompt("Yoshni kiriting:");
+
+// let tugilganYil = "2024 - yosh";
+// let oy = yosh * 12;
+// let hafta = ((yosh * 365) / 7).toFixed(0);
+// let kun = yosh * 365;
+// let soat = kun * 24;
+// let daqiqa = soat * 60;
+// let soniya = daqiqa * 60;
+
+// const result = `Qadirli ${ism}, Siz ${tugilganYil} yilda tug'ilgansiz.Va siz${oy} oy, ${hafta} hafta, ${kun}kun, ${soat} soat, ${daqiqa} daqiqa, ${soniya} soniya yashagansiz.😊 `;
+
+// alert(result);
+
+// 2-MASHQ
+
+// const ism=prompt('Ism kiriting')
+// const ismlar=('Shohruh','Sardor','Davron')
+
+// if(ismlar.includes(ism)){
+//     alert(`Ha ${ism} bor`)
+// }
+// else{alert(`Afsus ${ism} yo'q`)}
+
+// 3-MASHQ
+
+// const ismlar = ["Shohruh", "Sardor", "Davron"];
+
+// // FOR bilan
+
+// // for (let i = 0; i < ismlar.length; i++) {
+// //   let result = ismlar[i] + "bek";
+// //   console.log(result);
+// // }
+
+// let i = 0;
+
+// while (i < ismlar.length) {
+//   let result = ismlar[i] + "bek";
+//   console.log(result);
+//   i++;
+// }
+
+// 4-MASHQ
+
+// const viloyatNomi = prompt("Qaysi viloyat kerak ?");
+
+// SWITCH CASE bilan
+
+// switch (viloyatNomi) {
+//   case "Farg'ona":
+//     alert("Farg'ona viloyatida 2mln aholi bor");
+//     break;
+//   case "Andijon":
+//     alert("Andijon viloyatida 1.5mln aholi bor");
+//     break;
+//   case "Namangan":
+//     alert("Namangan viloyatida 1mln aholi bor");
+//     break;
+//     default:alert(`Hozircha ba'zada ${viloyatNomi} viloyat mavjud emas`)
+// }
+
+// IF bilan
+
+// if (viloyatNomi == "Farg'ona") {
+//   alert("Farg'ona viloyatida 2mln aholi bor");
+// } else if (viloyatNomi == "Andijon") {
+//   alert("Andijon viloyatida 1.5mln aholi bor");
+// } else if (viloyatNomi == "Namangan") {
+//   alert("Namangan viloyatida 1mln aholi bor");
+// } else {
+//   alert(`Hozircha ba'zada ${viloyatNomi} viloyat mavjud emas`);
+// }
+
+
+
